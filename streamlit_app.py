de5e369d54527bd573403db67b973a52a7d7b596
Sheldon Scott <104961355+SheldonScottDD@users.noreply.github.com>
2024-07-09 11:58:47 +0800

    Update streamlit_app.py
    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
-    "**Choose the fruits you want in your custom Smoothie!**"
+    "**Choose the fruits you want in your aaa custom Smoothie!**"
 )
 
 # option = st.selectbox(
@@ -24,7 +24,7 @@ st.write('The Name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 # st.dataframe(data=my_dataframe['FRUIT_NAME'], use_container_width=True)
 
 pd_df = my_dataframe.to_pandas()
